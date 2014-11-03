61de3446780b3a7ef2bfb476794cffbffabf9067
bl-test-account <plugin.developer@bytelight.com>
2014-11-03 17:07:54 -0500

    Released on: 2014-11-03 17:07:54 -0500

--- preview/page.js
+++ preview/page.js
@@ -20,3 +20,20 @@ $('button').on('click', function() {
   BL_INTERNAL.sendEventToNative('CAPTURE_IMAGE', {})
   return false;
 })
+
+BL_INTERNAL.listenForNative('IMAGE_CAPTURED', function() {
+  var d = new Date();
+  dateString = d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + 
+  formatAMPM(d);
+})
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
